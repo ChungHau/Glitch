e9a077f9a8405ea7f221bf46647c16cd7d95da59
Glitch (lesson3-query-parameters) <none>
2020-05-12 15:33:39 +0000

    🚊👘 Updated with Glitch

--- server.js
+++ server.js
@@ -10,15 +10,27 @@ app.set("view engine", "pug");
 app.set("views", "./views");
 
 // https://expressjs.com/en/starter/basic-routing.html
+var todos = ["Đi chợ", "Nấu cơm", "Rửa bát", "Học code tại CodersX"];
+
 app.get("/", (request, response) => {
   response.send("I love CodersX");
 });
 
-app.get("/todos", (request, response) => {
-  response.render("todos", {
-    todos: ["Đi chợ", "Nấu cơm", "Rửa bát", "Học code tại CodersX"]
+app.get("/todos", (req,res) => {
+  let q = req.query.q;
+  if(q) {
+    let matchedTodos = todos.filter(todo => todo.toLowerCase().indexOf(q.toLowerCase()) !== -1);
+    res.render("todos/index", {
+    todos: matchedTodos
+  });
+  }
+  
+  res.render("todos/index", {
+    todos: todos
   });
 });
+
+
 // listen for requests :)
 app.listen(process.env.PORT, () => {
   console.log("Server listening on port " + process.env.PORT);
