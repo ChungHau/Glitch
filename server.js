954a5aa6f95e2d12efe82b8c73e25fa8f8df28e3
Glitch (lesson-6-lowdb-database) <none>
2020-05-22 04:23:48 +0000

    🚛🏥 Updated with Glitch

--- server.js
+++ server.js
@@ -1,50 +1,41 @@
-// server.js
-// where your node app starts
-
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
-const express = require("express");
+const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser')
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
 
-app.set("view engine", "pug");
-app.set("views", "./views");
+const adapter = new FileSync('db.json')
+const db = low(adapter)
 
+app.set('view engine', 'pug')
+app.set('views', './views')
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 // https://expressjs.com/en/starter/basic-routing.html
-var todos = [
-  {id: 0, todo:"Đi chợ"}, 
-  {id: 1, todo:"Nấu cơm"},
-  {id: 2, todo:"Rửa bát"}, 
-  {id: 3, todo:"Học code tại CodersX"}
-];
-
-app.get("/", (request, response) => {
-  response.render("index");
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
-app.get("/todos", (req,res) => {
+app.get('/todos', (req, res) => {
   let q = req.query.q;
   if(q) {
-    let matchedTodos = todos.filter(todo => todo.toLowerCase().indexOf(q.toLowerCase()) !== -1);
-    res.render("todos/index", {
-    todos: matchedTodos
-  });
-  }
-  
-  res.render("todos/index", {
-    todos: todos
+    let matchedTodos = db.get('todos').value().filter(todo => todo.content.toLowerCase().indexOf(q.toLowerCase()) !== -1)
+    res.render('todos/index', {
+      todos: matchedTodos
+    });
+  } 
+  res.render('todos/index', {
+    todos: db.get('todos').value()
   });
 });
 
 app.post('/todos/create', (req,res) => {
-  todos.push(req.body);
+  db.get('todos').push(req.body).write();
   res.redirect('/todos')
-});
-
+})
 // listen for requests :)
 app.listen(process.env.PORT, () => {
   console.log("Server listening on port " + process.env.PORT);
 });
+// để mình check lại code
